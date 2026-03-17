13686783ca4312db4841efec3f07325296e505de
tukuyomi032 <118542180+tukuyomil032@users.noreply.github.com>
2026-03-18 08:05:42 +0900

    feat: modify lint-staged scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   'src/**/*.{ts,tsx}': (files) => [
-    `pnpm dlx @biomejs/biome check --write ${files.map((f) => `"${f}"`).join(' ')}`,
+    `biome check --write ${files.map((f) => `"${f}"`).join(' ')}`,
   ],
   'src-tauri/src/**/*.rs': () => 'cargo fmt --all --manifest-path src-tauri/Cargo.toml',
 };
